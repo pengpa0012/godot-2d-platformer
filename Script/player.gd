aa10044c93ebab963a3cf57599377196b2253555
Pengpa0012 <peng.ilikha@gmail.com>
2023-06-17 14:54:29 +0800

    added enemy

--- Script/player.gd
+++ Script/player.gd
@@ -2,14 +2,17 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+@onready var screen
 
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	print(get_viewport().get_visible_rect().size)
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+#		if self.position.x == 0 || self.position.x >= 1152:
+#			return
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
