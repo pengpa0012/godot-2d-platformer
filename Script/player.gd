290fb5d49f76c3fa12608fd021dd9c22a3e88d75
Pengpa0012 <peng.ilikha@gmail.com>
2023-06-17 21:28:01 +0800

    add deadzone

--- Script/player.gd
+++ Script/player.gd
@@ -10,8 +10,6 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-#		if self.position.x == 0 || self.position.x >= 1152:
-#			return
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
